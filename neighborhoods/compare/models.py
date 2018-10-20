d3cd792cc80923a715d36189aa971b1afc21d771
Jeremy Keeshin <jkeesh@gmail.com>
2018-10-20 14:36:26 -0400

    add gitignore update model

--- neighborhoods/compare/models.py
+++ neighborhoods/compare/models.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-
+@python_2_unicode_compatible
 class CityComparison(models.Model):
     """A model to save a single city/neighborhood comparison."""
 
@@ -15,3 +16,7 @@ class CityComparison(models.Model):
 
     timestamp = models.DateTimeField(auto_now_add=True)
     username = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.start_city + ":" + self.neighborhood1 + ", " \
+        + self.end_city + ":" + self.neighborhood2 + " (" + self.username + ")"
